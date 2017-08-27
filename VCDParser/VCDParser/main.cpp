7c8fcc8e353c66c9f68b9f21361a800f81483362
arjunagi <karthik.arjunagi@gmail.com>
2017-08-26 19:01:22 -0700

    testing jenkins

--- VCDParser/VCDParser/main.cpp
+++ VCDParser/VCDParser/main.cpp
@@ -7,7 +7,7 @@
 //
 
 /*
- * Server which receives the VCD data, parses it and stores it in a file and also in SQLite DB.
+ * Server which receives the VCD data, parses it and stores it in a file and also in SQLite database.
  */
 
 #include <iostream>
